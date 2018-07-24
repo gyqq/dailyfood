51c40c2ea5c0cda71b4dee875acd45af22736b4d
谷宇 <hbguyu@163.com>
2018-07-24 10:20:27 +0800

    备注,与新增

---
+++ src/pages/groupBuying/text.vue
@@ -0,0 +1,32 @@
+<style lang="less">
+
+</style>
+<template>
+
+</template>
+
+<script>
+  import wepy from 'wepy';
+  import { connect } from 'wepy-redux';
+
+  export default class text extends wepy.page {
+    config = {
+      navigationBarTitleText: 'text'
+    };
+    components = {};
+
+    mixins = [testMixin];
+
+    data = {};
+
+    computed = {};
+
+    methods = {};
+
+    events = {};
+
+    onLoad() {
+
+    }
+  }
+</script>
