034fe6ed87ea228ed3c35b49b807c80cf3ece040
谷宇 <hbguyu@163.com>
2018-08-30 17:56:55 +0800

    1.团购订单列表构建完成

--- src/mixins/util.js
+++ src/mixins/util.js
@@ -1,11 +1,20 @@
 import wepy from 'wepy'
-function getPublicData() {
-  var globalData = wepy.$instance.globalData
-  var access_token = wepy.getStorageSync('loginInfo').access_token ||''
-  var publicStr='?language='+globalData.language+'&platform='+globalData.platform+'&access_token='+access_token
-  console.log('公共参数获取')
-  return publicStr
+// function getPublicData() {
+//   var globalData = wepy.$instance.globalData
+//   var access_token = wepy.getStorageSync('loginInfo').access_token ||''
+//   var publicStr='?language='+globalData.language+'&platform='+globalData.platform+'&access_token='+access_token
+//   console.log('公共参数获取')
+//   return publicStr
+// }
+function clearStorageData(){
+  //订单确认页 confirmOrder
+  wepy.removeStorageSync('invoice')//清除发票信息
+  wepy.removeStorageSync('addr_id')//清除所选地址id
+  //团购详情页 groupDetails
+  wepy.removeStorageSync('imglist')//详情页存储的介绍信息
+
 }
 module.exports = {
-  getPublicData:getPublicData
+  // getPublicData:getPublicData
+  clearStorageData:clearStorageData
 }
