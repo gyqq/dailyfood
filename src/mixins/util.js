20e1a0deb2e49b7ab6d7ac2b9ed7bcf2ab1dafae
谷宇 <hbguyu@163.com>
2018-09-04 18:31:45 +0800

    1.团购订单详情交互

--- src/mixins/util.js
+++ src/mixins/util.js
@@ -33,8 +33,27 @@ function getzf(num){//补0操作
   return num;
 }
 //时间戳转时间格式==结束
+//强制保留两位小数
+function toDecimal2(x) {
+  var f = parseFloat(x);
+  if (isNaN(f)) {
+    return false;
+  }
+  var f = Math.round(x * 100) / 100;
+  var s = f.toString();
+  var rs = s.indexOf('.');
+  if (rs < 0) {
+    rs = s.length;
+    s += '.';
+  }
+  while (s.length <= rs + 2) {
+    s += '0';
+  }
+  return s;
+}
 module.exports = {
   // getPublicData:getPublicData
   clearStorageData:clearStorageData,
-  getMyDate:getMyDate
+  getMyDate:getMyDate,
+  toDecimal2:toDecimal2
 }
