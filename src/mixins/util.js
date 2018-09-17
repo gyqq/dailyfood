01193c99d73811cf499096bcd3e31b9d7994c251
谷宇 <hbguyu@163.com>
2018-09-17 19:11:20 +0800

    1.蹭团交互

--- src/mixins/util.js
+++ src/mixins/util.js
@@ -51,9 +51,20 @@ function toDecimal2(x) {
   }
   return s;
 }
+//时间戳转换时分秒
+function formatDuring(mss){
+  var hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = (mss % (1000 * 60)) / 1000;
+  hours = hours < 10 ? ('0' + hours) : hours;
+  minutes = minutes < 10 ? ('0' + minutes) : minutes;
+  seconds = seconds < 10 && seconds >= 1 ? ('0' + seconds) : seconds;
+  return  hours + " :" + minutes + " :" + seconds;
+}
 module.exports = {
   // getPublicData:getPublicData
   clearStorageData:clearStorageData,
   getMyDate:getMyDate,
-  toDecimal2:toDecimal2
+  toDecimal2:toDecimal2,
+  formatDuring:formatDuring
 }
