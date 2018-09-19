80f998c1f0a723912c2d1f2f86979ebd182727b8
谷宇 <hbguyu@163.com>
2018-09-19 19:22:20 +0800

    1.收货地址编辑

--- src/mixins/util.js
+++ src/mixins/util.js
@@ -59,12 +59,21 @@ function formatDuring(mss){
   hours = hours < 10 ? ('0' + hours) : hours;
   minutes = minutes < 10 ? ('0' + minutes) : minutes;
   seconds = seconds < 10 && seconds >= 1 ? ('0' + seconds) : seconds;
-  return  hours + " :" + minutes + " :" + seconds;
+  return  hours + ":" + minutes + ":" + seconds;
+}
+//剩余时分(团购订单详情)
+function formatDuringHM(mss){
+  var days = parseInt(mss / (1000 * 60 * 60 * 24));
+  var hours = parseInt((mss % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = parseInt((mss % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = (mss % (1000 * 60)) / 1000;
+  return  hours + "小时" + minutes + "分钟"+seconds+'秒';
 }
 module.exports = {
   // getPublicData:getPublicData
   clearStorageData:clearStorageData,
   getMyDate:getMyDate,
   toDecimal2:toDecimal2,
-  formatDuring:formatDuring
+  formatDuring:formatDuring,
+  formatDuringHM:formatDuringHM
 }
