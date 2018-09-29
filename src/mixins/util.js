72a78f041b217a9db9e01817f09c26b1f7ab0ce7
谷宇 <hbguyu@163.com>
2018-09-29 09:48:36 +0800

    1.秒杀列表完成

--- src/mixins/util.js
+++ src/mixins/util.js
@@ -69,11 +69,23 @@ function formatDuringHM(mss){
   var seconds = (mss % (1000 * 60)) / 1000;
   return  hours + "小时" + minutes + "分钟"+seconds+'秒';
 }
+//年月日时分秒
+function timestampToTime(timestamp) {
+  var date = new Date(timestamp * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  var Y = date.getFullYear() + '-';
+  var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+  var D = date.getDate() + ' ';
+  var h = date.getHours() + ':';
+  var m = (date.getMinutes() < 10 ? '0'+(date.getMinutes()) : date.getMinutes());
+  var s = (date.getSeconds() < 10 ? '0'+(date.getSeconds()) : date.getSeconds());
+  return Y+M+D+h+m;
+}
 module.exports = {
   // getPublicData:getPublicData
   clearStorageData:clearStorageData,
   getMyDate:getMyDate,
   toDecimal2:toDecimal2,
   formatDuring:formatDuring,
-  formatDuringHM:formatDuringHM
+  formatDuringHM:formatDuringHM,
+  timestampToTime:timestampToTime
 }
